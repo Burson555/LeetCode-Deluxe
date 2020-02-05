7b28a224034a7360d9111b2179b27b1361c8b990
Burson555 <by570@nyu.edu>
2020-02-04 23:49:57 -0800

    20200204

--- LC0017.java
+++ LC0017.java
@@ -38,3 +38,56 @@ class Solution {
         }
     }
 }
+
+
+// This method is even slower.
+// Runtime: 5 ms, faster than 19.68% of Java online submissions for Letter Combinations of a Phone Number.
+// Memory Usage: 37.8 MB, less than 6.16% of Java online submissions for Letter Combinations of a Phone Number.
+// Runtime: 5 ms, faster than 19.68% of Java online submissions for Letter Combinations of a Phone Number.
+// Memory Usage: 38.2 MB, less than 6.16% of Java online submissions for Letter Combinations of a Phone Number.
+
+class Solution {
+    
+    String tb[];
+    List<String> rl;
+    
+    public Solution() {
+        tb = new String[10];
+        tb[2] = "abc";
+        tb[3] = "def";
+        tb[4] = "ghi";
+        tb[5] = "jkl";
+        tb[6] = "mno";
+        tb[7] = "pqrs";
+        tb[8] = "tuv";
+        tb[9] = "wxyz";
+        rl = new LinkedList<String>();
+    }
+    
+    public List<String> letterCombinations(String digits) {
+        if (digits == null || digits.length() == 0) return this.rl;
+        int curr = 0;
+        String prev = "";
+        while (curr < digits.length()) {
+            while (prev.compareTo("") == 0 || prev.length() <= curr){
+                String temp = tb[digits.charAt(curr) - '0'];
+                for (int i = 0; i < temp.length(); i++)
+                    rl.add(prev + temp.charAt(i));
+                prev = rl.remove(0);
+            }
+            curr++;
+        }
+        rl.add(0, prev);
+        return this.rl;
+    }
+    
+    private void create(String s, String digits, int curr) {
+        if (curr == digits.length()) {
+            this.rl.add(s);
+        } else {
+            String temp = tb[digits.charAt(curr) - '0'];
+            for (char c : temp.toCharArray())
+                this.create(s+c, digits, curr+1);
+        }
+    }
+}
--- LC0384.java
+++ LC0384.java
@@ -1,33 +1,31 @@
-// Runtime: 75 ms, faster than 73.49% of Java online submissions for Shuffle an Array.
-// Memory Usage: 51.5 MB, less than 100.00% of Java online submissions for Shuffle an Array.
-// Runtime: 74 ms, faster than 76.83% of Java online submissions for Shuffle an Array.
-// Memory Usage: 51.2 MB, less than 100.00% of Java online submissions for Shuffle an Array.
-// Runtime: 76 ms, faster than 69.98% of Java online submissions for Shuffle an Array.
-// Memory Usage: 51.5 MB, less than 100.00% of Java online submissions for Shuffle an Array.
-// Runtime: 77 ms, faster than 66.84% of Java online submissions for Shuffle an Array.
+// Not significantly faster this way, but the code is simpler.
+
+// Runtime: 80 ms, faster than 60.68% of Java online submissions for Shuffle an Array.
+// Memory Usage: 50.9 MB, less than 100.00% of Java online submissions for Shuffle an Array.
+// Runtime: 84 ms, faster than 50.41% of Java online submissions for Shuffle an Array.
+// Memory Usage: 50.6 MB, less than 100.00% of Java online submissions for Shuffle an Array.
+// Runtime: 77 ms, faster than 68.87% of Java online submissions for Shuffle an Array.
 // Memory Usage: 51.1 MB, less than 100.00% of Java online submissions for Shuffle an Array.
+
+// Note how we use clone function in this new version.
+// clone() just creates another chunk of memory with the same values.
 
 import java.util.Random;
 
 class Solution {
     
     int[] arr;
-    int[] ori;
+    private int[] ori;
     
     public Solution(int[] nums) {
-        this.arr = new int[nums.length];
-        this.ori = new int[nums.length];
-        for (int i = 0; i < nums.length; i++){
-            this.arr[i] = nums[i];
-            this.ori[i] = nums[i];
-        }
+        this.arr = nums;
+        this.ori = nums.clone();
     }
     
     /** Resets the array to its original configuration and return it. */
     public int[] reset() {
-        for (int i = 0; i < this.arr.length; i++) {
-            this.arr[i] = this.ori[i];
-        }
+        this.arr = this.ori;
+        this.ori = this.ori.clone();
         return this.arr;
     }
     
@@ -36,7 +34,7 @@ class Solution {
         Random r = new Random();
         int len = this.arr.length;
         for (int i = 0; i < len; i++) {
-            int j = r.nextInt(len);
+            int j = r.nextInt(len-i) + i;
             int temp = this.arr[j];
             this.arr[j] = this.arr[i];
             this.arr[i] = temp;
@@ -51,3 +49,59 @@ class Solution {
  * int[] param_1 = obj.reset();
  * int[] param_2 = obj.shuffle();
  */
+
+
+// // Runtime: 75 ms, faster than 73.49% of Java online submissions for Shuffle an Array.
+// // Memory Usage: 51.5 MB, less than 100.00% of Java online submissions for Shuffle an Array.
+// // Runtime: 74 ms, faster than 76.83% of Java online submissions for Shuffle an Array.
+// // Memory Usage: 51.2 MB, less than 100.00% of Java online submissions for Shuffle an Array.
+// // Runtime: 76 ms, faster than 69.98% of Java online submissions for Shuffle an Array.
+// // Memory Usage: 51.5 MB, less than 100.00% of Java online submissions for Shuffle an Array.
+// // Runtime: 77 ms, faster than 66.84% of Java online submissions for Shuffle an Array.
+// // Memory Usage: 51.1 MB, less than 100.00% of Java online submissions for Shuffle an Array.
+
+// import java.util.Random;
+
+// class Solution {
+    
+//     int[] arr;
+//     int[] ori;
+    
+//     public Solution(int[] nums) {
+//         this.arr = new int[nums.length];
+//         this.ori = new int[nums.length];
+//         for (int i = 0; i < nums.length; i++){
+//             this.arr[i] = nums[i];
+//             this.ori[i] = nums[i];
+//         }
+//     }
+    
+//     /** Resets the array to its original configuration and return it. */
+//     public int[] reset() {
+//         for (int i = 0; i < this.arr.length; i++) {
+//             this.arr[i] = this.ori[i];
+//         }
+//         return this.arr;
+//     }
+    
+//     /** Returns a random shuffling of the array. */
+//     public int[] shuffle() {
+//         Random r = new Random();
+//         int len = this.arr.length;
+//         for (int i = 0; i < len; i++) {
+//             // It will be easier to calculate the probabilities if we derive j this way
+//             int j = r.nextInt(len-i) + i;
+//             int temp = this.arr[j];
+//             this.arr[j] = this.arr[i];
+//             this.arr[i] = temp;
+//         }
+//         return this.arr;
+//     }
+// }
+
+// /**
+//  * Your Solution object will be instantiated and called as such:
+//  * Solution obj = new Solution(nums);
+//  * int[] param_1 = obj.reset();
+//  * int[] param_2 = obj.shuffle();
+//  */
